6fc44f7df8d2601c0ecfbab76db600381c29cbc6
Jason Sher <orbitbreak@gmail.com>
2012-10-02 01:00:09 -0400

    bugfixes

--- tictactoe-hardmode.py
+++ tictactoe-hardmode.py
@@ -8,23 +8,20 @@ from Tkinter import *
 textsize = 16
 
 class TicTacToe(object):
-	# possible ways to win TTT
+
     winners = ({1, 2, 3}, {4, 5, 6}, {7, 8, 9},
                     {1, 4, 7}, {2, 5, 8}, {3, 6, 9},
                     {1, 5, 9}, {3, 5, 7})
 
     def __init__(self, top):
-        self.player = bool(random.randint(0,1))  # randomize who starts
-        self.display(('Player = O\nHumanKiller9000 = X\nHK9k\'s Move.' if self.player
-                     else 'Player = O\nHumanKiller9000 = X\nMake Your Move.') +
-                     '\nClose to meet your maker.\n')
-
-        self.button_dic = {}   # pointer to buttons and StringVar()
+        self.player = bool(random.randint(0,1))
+		
+        self.button_dic = {}   # buttons and StringVar()
         self.top = top
         self.top.geometry("240x240+20+20")
         self.top.wm_attributes('-topmost', True)
 
-        self.X_O_dict = {"X":[], "O":[]}  # list of "X" and "O" moves
+        self.X_O_dict = {"X":[], "O":[]}  # moves
         self.top.title('TicTacToe Hard Mode')
         self.top_frame = Frame(self.top, width = 240, height = 240)
         self.top_frame.grid(row = 0, column = 1)
@@ -40,7 +37,7 @@ class TicTacToe(object):
 
     def play(self):
         while self.moves < 9 and self.tie:
-            self.selection()
+            self.choice()
         try:
             self.stop()
         except TclError:
@@ -70,11 +67,10 @@ class TicTacToe(object):
     def stop(self):
           
         if not self.tie:
-            self.display("Winner is O" if self.player else "Winner is X")
+            self.draw("gg O" if self.player else "gg X")
         elif self.moves == 9:
-            self.display("You survived. This time." )
+            self.draw("Nice." )
         else:
-            self.display("Only a matter of time.")
             self.moves = 99
             self.tie = False
 
@@ -108,7 +104,7 @@ class TicTacToe(object):
         if any(winner.issubset(set_in) for winner in self.winners):
             self.tie = False
 
-    def check_two_in_a_row(self):
+    def twocheck(self):
         # check to win first and then to block
         for player in ["X", "O"]:
             this = set(self.X_O_dict[player])
@@ -119,7 +115,7 @@ class TicTacToe(object):
                     if self.legal_move(one_to_return):
                         return one_to_return
 
-    def display(self, msg, title='Instructions'):
+    def draw(self, msg, title='Instructions'):
         tl=Toplevel()
         tl.geometry("300x300+30+60")
         tl.title(title)
@@ -133,19 +129,15 @@ class TicTacToe(object):
         return (square_number not in self.X_O_dict["X"] and
                    square_number not in self.X_O_dict["O"])
 
-    def selection(self):
-        # computer moves
+    def choice(self):
+        # a.i.
         if self.player:
-            # don't accept button clicks when it is computer's (X) turn
             for but in self.button_dic:
                 self.button_dic[but][1].state = DISABLED
-
-            move_to_take = self.check_two_in_a_row()
+            move_to_take = self.twocheck()
             if move_to_take is not None:
                 self.cb_handler(move_to_take)
             else:
-                # sequence = middle square, and then each corner as the
-                # 2 middle rows are elmininated by the middle square
                 for chosen in (5, 1, 3, 7, 9, 2, 4, 6, 8):
                     if self.legal_move(chosen):
                         self.cb_handler(chosen)
